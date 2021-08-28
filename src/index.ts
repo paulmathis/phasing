0067c98fa88613f26ac750e13920e62e532a2fc8
Paul Mathis <pmathis@Pauls-MBP.lan>
2021-08-27 21:44:47 -0500

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import Phaser from "phaser";
+
+class MainScene extends Phaser.Scene {
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+  }
+
+  update() {}
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [MainScene],
+};
+
+class Game extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
+
+window.addEventListener("load", () => {
+  const game = new Phaser.Game(config);
+});
